7b7012e564a90f62c700b07bcdc681f0f8c6f3fe
wangjessicax <wang.jessicax@gmail.com>
2018-04-11 10:47:19 -0400

    squash

---
+++ health.py
@@ -0,0 +1,44 @@
+
+
+def loader():
+
+	file = open("data.txt","r") 
+	contents=file.read()
+		
+		
+
+		#print(contents)
+		#print(contents)
+		#creates array with each data set 
+	totalList=list()
+	for x in range(0, 30000):
+		totalList.append(contents[x*2033:(x+1)*2033])
+
+		#creates second array with specifically depression data which is in column 113 
+	depList=list()
+	for x in range(0,60346):
+		depList.append(contents[(x*2033)+113])
+	print(depList)
+
+def test_loader():
+	file = open("data.txt","r") 
+	contents=file.read()
+		
+		
+
+		#print(contents)
+		#print(contents)
+		#creates array with each data set 
+	totalList=list()
+	for x in range(0, 30346):
+		totalList.append(contents[x*2033:(x+1)*2033])
+
+		#creates second array with specifically depression data which is in column 113 
+	depList=list()
+	for x in range(0,60346):
+		depList.append(contents[(x*2033)+113])
+	print(depList)
+
+
+
+
