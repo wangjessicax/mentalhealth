3a34c6b720c3717853850695006b914c4c7c4159
wangjessicax <wang.jessicax@gmail.com>
2018-04-28 16:05:32 -0400

    fdafdas

--- network3.py
+++ network3.py
@@ -115,9 +115,9 @@ def get_data():
     M=1
     print("Total list...:")
     print(totalList_array[0].shape)
-    all_X = np.ones((N, M + 1))
+    #all_X = np.ones((N, M + 1))
     print(all_x[0].shape)
-    all_X[:, 1:] = np.reshape(totalList_array, (60346, ))
+    all_X = np.reshape(totalList_array, (60346,2033))
 
 
     # Convert into one-hot vectors
