8e33b5792cb354f68d24d65ce2336e7510787449
wangjessicax <wang.jessicax@gmail.com>
2018-04-28 14:28:26 -0400

    fdsaafd

--- network3.py
+++ network3.py
@@ -54,7 +54,7 @@ def get_data():
         print(s)
         charList = list()
         for c in s:
-            if c.isspace() is true:  # if string is empty
+            if c.isspace() is True:  # if string is empty
                 charList.append(-1)
             if c:
                 char=float(c)
