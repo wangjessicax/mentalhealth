342da8fc61de0168e6489623e145c84a5ef11a1c
wangjessicax <wang.jessicax@gmail.com>
2018-04-28 14:12:13 -0400

    fda

--- network3.py
+++ network3.py
@@ -50,6 +50,7 @@ def get_data():
     #cycle through string and convert into array
     
     for s in totalList:
+        print("this is the string")
         print(s)
         charList = list()
         for c in s:
